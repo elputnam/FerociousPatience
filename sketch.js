1201db8930632dcb878858333ecf56f52c9f0035
elputnam <elputnam@inaction.ie>
2021-11-28 15:17:05 +0000

    relative sqaure canvas

--- sketch.js
+++ sketch.js
@@ -49,8 +49,13 @@ function preload(){
 
 function setup() {
   //createCanvas(windowWidth, windowHeight);
-  createCanvas(600, 600);
-  // createCanvas(windowWidth, windowWidth);
+  // createCanvas(600, 600);
+  if (windowWidth > windowHeight){
+    createCanvas(windowHeight, windowHeight);
+  } else {
+    createCanvas(windowWidth, windowWidth);
+  }
+
   colorMode(HSB, 360, 100, 100, 100);
   background(0, 100, 10);
   frameRate(15);
